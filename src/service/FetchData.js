2db8b730a822cc813581f3265d333918c809a620
nickiskit <nickiskit@gmail.com>
2020-10-15 04:30:02 +0700

    First workshop day: add forgotten files

---
+++ src/service/FetchData.js
@@ -0,0 +1,26 @@
+export default class FetchData {
+
+	startUrl = 'https://api.spacexdata.com/v4';
+
+	getResource = async url => {
+		const res = await fetch(url);
+
+		if (!res.ok) {
+			throw new Error(`Error: ${res.status}`);
+		}
+
+		return await res.json();
+	};
+
+	getRocket =  async () => {
+		return await this.getResource(this.startUrl + '/rockets');
+	};
+
+	getLaunches = async () => {
+		return await this.getResource(this.startUrl + '/launches/past');
+	};
+
+	getCompany = async () => {
+		return await this.getResource(this.startUrl + '/company');
+	};
+}
